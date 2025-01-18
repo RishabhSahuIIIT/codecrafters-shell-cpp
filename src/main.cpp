aff2bf736c5d571e19096447946666e3f433e191
Rishabh Sahu <rishabhsahu325@gmail.com>
2025-01-18 09:41:51 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,84 @@
 #include <iostream>
 #include<cstdlib>
 #include<sstream>
+#include<cstring>
+#include<vector>
+#include<filesystem>
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << std::unitbuf;
+  cerr << std::unitbuf;
 
   while(true)
   {
-    std::cout << "$ ";
+    cout << "$ ";
     
-    std::string input;
-    std::getline(std::cin, input);
+    string input;
+    getline(std::cin, input);
     if(input.substr(0,4)=="exit")
+    {
       exit(0);
+    }
     else if(input.substr(0,4)=="echo")
-      std::cout<<input.substr(5,input.size()-5)<<"\n";
+    { 
+      cout<<input.substr(5,input.size()-5)<<"\n";
+    }
     else if(input.substr(0,4)=="type")
     {
-      std::stringstream ss;
-      std:: string arg1;
-      std:: string arg2;
-      std:: string arg3;
+      stringstream ss;
+       string arg1;
+       string arg2;
+       string arg3;
       ss<<input;
       ss>>arg1>>arg2;
+      bool flag=false;
+      string paths=string(getenv("PATH"));
+        stringstream tokenizer(paths);
+        string token;
+        vector<string>pathVars;
+        while(getline(tokenizer,token,':'))
+        {
+          pathVars.push_back(token);
+        }
+        
+        
+        //search executable iterated in a loop over path strings
+        for(string pathFolder :pathVars )
+        {
+            string totalPath= pathFolder+'/'+arg2;
+            filesystem::path pth(totalPath);
+            
+
+            //parse the path variable into a vector of strings,
+        
+            //set flag and break the loop if condition met
+            //check flag and output at the end accordingly
+
+            flag=flag | (filesystem::exists(pth))    ;
+            
+
+        }
+        
       if(arg2=="echo" or arg2=="exit" or arg2=="type")
       {
-        std::cout<<arg2<<" is a shell builtin\n";
+        cout<<arg2<<" is a shell builtin\n";
 
+      }
+      else if(flag) //check if command is present in any of the directories of the path variable
+      {
+        
       }
       else
       {
-        std::cout<<arg2<<": not found\n";
+        cout<<arg2<<": not found\n";
       }
 
     }
     else
-      std::cout<<input<<": command not found\n";
+    {
+      cout<<input<<": command not found\n";
+    }
     
 
     
