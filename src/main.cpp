3f55d9c902cda0fc3afd22731744cf193f582f68
Rishabh Sahu <rishabhsahu325@gmail.com>
2025-01-20 14:39:57 +0000

    runsExternalCommand

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,16 @@ void executeCommand(string mainCommand,vector<char*>argList)
   
   int sz=argList.size();
 
-  //char * const *  argListC = new char * const[sz+1]
   int pos=0;
-  //for(string arg:argList)
-  //{
-    //cout<<arg.c_str();
-    //argListC[pos] = (char*)malloc(arg.length() + 1);
-    //strcpy(argListC[pos],arg.c_str());
-    //cout<<argListC[pos];
-    //pos++;
-  //}
   int processPid= fork();
-  //cerr<<mainCommand;
   if(processPid==0)//child
   { 
-    
     execvp(mainCommand.c_str(),argList.data());
-    
   }
   else if (processPid>0) //parent
   {
     int status;
     waitpid(processPid, &status, 0);
-    
-    
   }
   else // process pid ==-1 or error
   {
@@ -62,96 +48,99 @@ int main() {
     {
       exit(0);
     }
-    else if(input.substr(0,4)=="echo")
+    else if(input.substr(0,4)=="echo")// basic echo without command substitution
     { 
       cout<<input.substr(5,input.size()-5)<<"\n";
+    }
+    else if(input.substr(0,3)=="pwd")
+    {
+      string directory;
+      getcwd(directory.data(),0);
+      cout<<directory;
     }
     else
     {
       stringstream ss;
-       string arg1;
-       string arg2;
-       string arg3;
+      string arg1;
+      string arg2;
+      string arg3;
       ss<<input;
       ss>>arg1>>arg2;
       bool flag=false;
       string paths=string(getenv("PATH"));
       
-        stringstream tokenizer(paths);
-        string token;
-        vector<string>pathVars;
-        string detectedPathString;
-        //parse the path variable into a vector of strings,
-        
-            //set flag and break the loop if condition met
-            //check flag and output at the end accordingly
+      stringstream tokenizer(paths);
+      string token;
+      vector<string>pathVars;
+      string detectedPathString;
+      //parse the path variable into a vector of strings,
+      
+      //set flag and break the loop if condition met
+      //check flag and output at the end accordingly
 
-        while(getline(tokenizer,token,':'))
-        {
-          pathVars.push_back(token);
-        }
+      while(getline(tokenizer,token,':'))
+      {
+        pathVars.push_back(token);
+      }
+      
         
-        
-        //search executable iterated in a loop over path strings
-        for(string pathFolder :pathVars )
-        {
-            string totalPath;
-            if(arg1=="type")
-              totalPath= pathFolder+'/'+arg2;
-            else
-              totalPath= pathFolder+'/'+arg1;
-            
-            filesystem::path pth(totalPath);
-            
-            flag=flag | (filesystem::exists(pth))    ;
-            if(filesystem::exists(pth))
-            {
-              detectedPathString=totalPath;
-              
-              break;
-            }
-            
-
-        }
-
-        
-        if(input.substr(0,4)=="type")
-        {
-          if(arg2=="echo" or arg2=="exit" or arg2=="type")
-          {
-            cout<<arg2<<" is a shell builtin\n";
-
-          }
-          else if(flag)
-          {
-  
-              cout<<arg2<<" is "<<detectedPathString<<"\n";
-          }
+      //search executable iterated in a loop over path strings
+      for(string pathFolder :pathVars )
+      {
+          string totalPath;
+          if(arg1=="type")
+            totalPath= pathFolder+'/'+arg2;
           else
-          {
-            cout<<arg2<<": not found\n";
-          }
-        }   
-        else if(flag==false)
-        {
-          cout<<input<<": command not found\n";
-        } 
-        else // command detected in path and needs to be executed from argument list
-        {
+            totalPath= pathFolder+'/'+arg1;
           
-          vector<char*> argumentsList;
-          //cout<<arg2; //printed name parameter 
-          argumentsList.push_back(const_cast<char*>(arg1.c_str()));
-          argumentsList.push_back(const_cast<char*>(arg2.c_str()));//arguments list apart from the main command
-          string par;
-          while(!ss.eof())
+          filesystem::path pth(totalPath);
+          
+          flag=flag | (filesystem::exists(pth))    ;
+          if(filesystem::exists(pth))
           {
-            ss>>par;
-            argumentsList.push_back(const_cast<char*>(par.c_str()));
-          }
-          executeCommand(arg1,argumentsList);
+            detectedPathString=totalPath;
+            break;
+          }            
+      }
+
+      
+      if(input.substr(0,4)=="type")
+      {
+        if(arg2=="echo" or arg2=="exit" or arg2=="type")
+        {
+          cout<<arg2<<" is a shell builtin\n";
+
         }
-    //cout<<"another output\n";
+        else if(flag)
+        {
+
+            cout<<arg2<<" is "<<detectedPathString<<"\n";
+        }
+        else
+        {
+          cout<<arg2<<": not found\n";
+        }
+      }   
+      else if(flag==false)
+      {
+        cout<<input<<": command not found\n";
+      } 
+      else // command detected in path and needs to be executed from argument list
+      {
+        
+        vector<char*> argumentsList;
+        //cout<<arg2; //printed name parameter 
+        argumentsList.push_back(const_cast<char*>(arg1.c_str()));
+        argumentsList.push_back(const_cast<char*>(arg2.c_str()));//arguments list apart from the main command
+        string par;
+        while(!ss.eof())
+        {
+          ss>>par;
+          argumentsList.push_back(const_cast<char*>(par.c_str()));
+        }
+        executeCommand(arg1,argumentsList);
+      }
+  
 
     
     
