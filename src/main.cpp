a99afe526d6cced93caec4c405da8e6d93f0fe50
Rishabh Sahu <rishabhsahu325@gmail.com>
2025-02-14 13:52:56 +0000

    quotedExecutableRemoveEscapes7

--- src/main.cpp
+++ src/main.cpp
@@ -491,7 +491,7 @@ int main() {
         else
         {
           arg1=command.substr(1,sz2-1);
-          arg1=unescapeWord(arg1);
+          //arg1=unescapeWord(arg1);
 //cout<<"("<<arg1<<")\n";
           argString= input.substr(sz2+1);
 
